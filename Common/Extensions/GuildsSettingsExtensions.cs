c9ad7e234e01789453a63f1f51b5e681a0b18f99
emre1702 <emre1702@live.de>
2021-02-11 00:19:34 +0100

    Add extensions for easier GuildsSettings access

---
+++ Common/Extensions/GuildsSettingsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BonusBot.Database.Entities.Settings;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BonusBot.Common.Extensions
+{
+    public static class GuildsSettingsExtensions
+    {
+        public static Task<GuildsSettings?> Get(this DbSet<GuildsSettings> dbSet, ulong guildId, string key, string moduleName)
+        {
+            moduleName = moduleName.ToModuleName();
+            return dbSet.FirstOrDefaultAsync(s =>
+               s.GuildId == guildId &&
+               EF.Functions.Like(s.Key, key) &&
+               EF.Functions.Like(s.Module, moduleName)) as Task<GuildsSettings?>;
+        }
+
+        public static GuildsSettings Create(this DbSet<GuildsSettings> dbSet, ulong guildId, string key, string moduleName, string value)
+        {
+            var settings = new GuildsSettings { GuildId = guildId, Module = moduleName, Key = key, Value = value };
+            dbSet.Add(settings);
+            return settings;
+        }
+    }
+}
