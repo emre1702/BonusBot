6b9cefee87fe67cdebd40e79f582b8bfebe6e753
emre1702 <emre1702@live.de>
2021-02-10 01:17:35 +0100

    Remove old comment

--- Modules/GamePlaningModule/Settings.cs
+++ Modules/GamePlaningModule/Settings.cs
@@ -4,9 +4,7 @@ namespace BonusBot.GamePlaningModule
 {
     public class Settings : IGuildSettingsConstantProperties
     {
-        //public const string AnnouncementChannelId = "AnnouncementChannelId";
         public const string AnnouncementEmoteId = "AnnouncementEmoteId";
-
         public const string AnnouncementMentionEveryone = "AnnouncementMentionEveryone";
     }
 }
