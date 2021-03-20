77bc2be1c1cabf7cfb49ffb198747e95cb7b5ddc
emre1702 <emre1702@live.de>
2021-03-20 08:53:06 +0100

    Comment out https redirection and hsts

--- Modules/WebDashboardBoardModule/Startup.cs
+++ Modules/WebDashboardBoardModule/Startup.cs
@@ -46,8 +46,8 @@ namespace WebDashboardBoard
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                app.UseHttpsRedirection();
+                //app.UseHsts();
+                //app.UseHttpsRedirection();
             }
             app.UseStaticFiles();
             if (!env.IsDevelopment())
