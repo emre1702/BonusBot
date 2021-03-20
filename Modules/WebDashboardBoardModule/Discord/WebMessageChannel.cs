36d67b0d8bcfd30b9ee7890ba66cd7699b090666
emre1702 <emre1702@live.de>
2021-03-20 08:31:06 +0100

    Fix nullable warnings

--- Modules/WebDashboardBoardModule/Discord/WebMessageChannel.cs
+++ Modules/WebDashboardBoardModule/Discord/WebMessageChannel.cs
@@ -100,7 +100,7 @@ namespace BonusBot.WebDashboardBoardModule.Discord
             throw new NotImplementedException();
         }
 
-        public Task ModifyAsync(Action<GuildChannelProperties> func, RequestOptions options = null)
+        public Task ModifyAsync(Action<GuildChannelProperties> func, RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
@@ -115,22 +115,22 @@ namespace BonusBot.WebDashboardBoardModule.Discord
             throw new NotImplementedException();
         }
 
-        public Task RemovePermissionOverwriteAsync(IRole role, RequestOptions options = null)
+        public Task RemovePermissionOverwriteAsync(IRole role, RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task RemovePermissionOverwriteAsync(IUser user, RequestOptions options = null)
+        public Task RemovePermissionOverwriteAsync(IUser user, RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task AddPermissionOverwriteAsync(IRole role, OverwritePermissions permissions, RequestOptions options = null)
+        public Task AddPermissionOverwriteAsync(IRole role, OverwritePermissions permissions, RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task AddPermissionOverwriteAsync(IUser user, OverwritePermissions permissions, RequestOptions options = null)
+        public Task AddPermissionOverwriteAsync(IUser user, OverwritePermissions permissions, RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
@@ -145,12 +145,12 @@ namespace BonusBot.WebDashboardBoardModule.Discord
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(RequestOptions options = null)
+        public Task DeleteAsync(RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task LeaveAsync(RequestOptions options = null)
+        public Task LeaveAsync(RequestOptions? options = null)
         {
             throw new NotImplementedException();
         }
