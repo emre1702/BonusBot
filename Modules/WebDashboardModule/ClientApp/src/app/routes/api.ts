a0b5c830fdbbc6457a708cd09f781f65888921ba
emre1702 <emre1702@live.de>
2021-03-21 21:13:03 +0100

    Revert webpack hack, use relative path for api

--- Modules/WebDashboardModule/ClientApp/src/app/routes/api.ts
+++ Modules/WebDashboardModule/ClientApp/src/app/routes/api.ts
@@ -1,25 +1,21 @@
-import { environment } from 'src/environments/environment';
-
-const root = environment.apiBaseUrl;
-
 export default {
     get: {
         command: {
-            volume: `${root}/WebCommand/Volume`,
+            volume: `WebCommand/Volume`,
         },
         content: {
-            accessLevel: `${root}/Content/UserAccessLevel`,
+            accessLevel: `Content/UserAccessLevel`,
         },
         navigation: {
-            guilds: `${root}/Navigation/Guilds`,
+            guilds: `Navigation/Guilds`,
         },
         login: {
-            oAuthUrl: `${root}/Login/OAuthUrl`,
+            oAuthUrl: `Login/OAuthUrl`,
         },
     },
     post: {
         command: {
-            execute: `${root}/WebCommand/Execute`,
+            execute: `WebCommand/Execute`,
         },
     },
 };
