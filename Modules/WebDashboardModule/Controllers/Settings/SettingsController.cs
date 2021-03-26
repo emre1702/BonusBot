7721dfb4f034129c17e13406c011d8a0e071c453
emre1702 <emre1702@live.de>
2021-03-26 16:19:11 +0100

    Put logic into service instead of controller

--- Modules/WebDashboardModule/Controllers/Settings/SettingsController.cs
+++ Modules/WebDashboardModule/Controllers/Settings/SettingsController.cs
@@ -14,25 +14,19 @@ namespace BonusBot.WebDashboardModule.Controllers.Settings
     [Route("[controller]")]
     public class SettingsController : Controller
     {
-        private readonly IModulesHandler _modulesHandler;
-        private readonly IGuildsHandler _guildsHandler;
         private readonly UserValidationService _userValidationService;
+        private readonly SettingsService _settingsService;
 
-        public SettingsController(IModulesHandler modulesHandler, IGuildsHandler guildsHandler)
-            => (_modulesHandler, _guildsHandler, _userValidationService) = (modulesHandler, guildsHandler, new());
+        public SettingsController(IGuildsHandler guildsHandler, IModulesHandler modulesHandler)
+            => (_userValidationService, _settingsService) = (new(), new(guildsHandler, modulesHandler));
 
         [HttpGet("AllModuleDatas")]
         public ActionResult<IEnumerable<ModuleData>> GetAllModuleDatas([FromQuery] string guildId)
         {
             _userValidationService.AssertIsInGuild(HttpContext.Session, guildId);
 
-            var bonusGuild = _guildsHandler.GetGuild(ulong.Parse(guildId))!;
-
-            var allAssemblyDatas = _modulesHandler.LoadedModuleAssemblies
-                .Where(SettingsHelper.HasSettings)
-                .Select(a => new ModuleData(a.ToModuleName(), bonusGuild.Modules.Contains(a)))
-                .OrderBy(a => a.Name);
-            return Ok(allAssemblyDatas);
+            var moduleDatas = _settingsService.GetModuleDatas(guildId);
+            return Ok(moduleDatas);
         }
 
         [HttpGet("ModuleSettings")]
@@ -40,15 +34,8 @@ namespace BonusBot.WebDashboardModule.Controllers.Settings
         {
             _userValidationService.AssertIsInGuild(HttpContext.Session, guildId);
 
-            var bonusGuild = _guildsHandler.GetGuild(ulong.Parse(guildId))!;
-            var assembly = bonusGuild.Modules.GetActivatedModuleAssembly(moduleName);
-            if (assembly is null) return Ok(Enumerable.Empty<ModuleSetting>());
-
-            var settings = SettingsHelper.GetSettingKeys(assembly).Select(key => new ModuleSetting(key)).ToList();
-            foreach (var setting in settings)
-                setting.Value = (await bonusGuild.Settings.Get<object?>(assembly, setting.Name))?.GetIdentifier() ?? string.Empty;
-
-            return Ok(settings);
+            var moduleSettings = await _settingsService.GetModuleSettings(guildId, moduleName);
+            return Ok(moduleSettings);
         }
     }
 }
--- Modules/WebDashboardModule/Services/SettingsService.cs
+++ Modules/WebDashboardModule/Services/SettingsService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BonusBot.Common.Extensions;
+using BonusBot.Common.Helper;
+using BonusBot.Common.Interfaces.Guilds;
+using BonusBot.Common.Interfaces.Services;
+using BonusBot.WebDashboardModule.Models.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,17 +12,34 @@ namespace BonusBot.WebDashboardModule.Services
 {
     public class SettingsService
     {
-        /*public async Task<IActionResult> GetAudioSettingsState([FromQuery] string guildId)
+        private readonly IGuildsHandler _guildsHandler;
+        private readonly IModulesHandler _modulesHandler;
+
+        public SettingsService(IGuildsHandler guildsHandler, IModulesHandler modulesHandler)
+            => (_guildsHandler, _modulesHandler) = (guildsHandler, modulesHandler);
+
+        public IEnumerable<ModuleData> GetModuleDatas(string guildId)
         {
-            _userValidationService.AssertIsInGuild(HttpContext.Session, guildId);
+            var bonusGuild = _guildsHandler.GetGuild(ulong.Parse(guildId))!;
 
-            var webCommandService = new WebCommandService(_guildsHandler, _discordClientHandler, _commandsHandler, _mainServiceProvider);
-            var messages = await webCommandService.Execute(HttpContext.Session, guildId, "GetState");
+            var allAssemblyDatas = _modulesHandler.LoadedModuleAssemblies
+                .Where(SettingsHelper.HasSettings)
+                .Select(a => new ModuleData(a.ToModuleName(), bonusGuild.Modules.Contains(a)))
+                .OrderBy(a => a.Name);
+            return allAssemblyDatas;
+        }
 
-            var json = messages.FirstOrDefault(m => m.StartsWith("{"));
-            if (json is not null)
-                return Ok(json);
-            return Ok("{}");
-        }*/
+        public async Task<IEnumerable<ModuleSetting>> GetModuleSettings(string guildId, string moduleName)
+        {
+            var bonusGuild = _guildsHandler.GetGuild(ulong.Parse(guildId))!;
+            var assembly = bonusGuild.Modules.GetActivatedModuleAssembly(moduleName);
+            if (assembly is null) return Enumerable.Empty<ModuleSetting>();
+
+            var settings = SettingsHelper.GetSettingKeys(assembly).Select(key => new ModuleSetting(key)).ToList();
+            foreach (var setting in settings)
+                setting.Value = (await bonusGuild.Settings.Get<object?>(assembly, setting.Name))?.GetIdentifier() ?? string.Empty;
+
+            return settings;
+        }
     }
 }
