2c4791c249062afe887ac028a881ac3e2fa34d99
emre1702 <emre1702@live.de>
2021-03-27 13:19:56 +0100

    Fix missing Common settings in Settings web dashboard

--- Modules/WebDashboardModule/Services/SettingsService.cs
+++ Modules/WebDashboardModule/Services/SettingsService.cs
@@ -1,4 +1,5 @@
-﻿using BonusBot.Common.Extensions;
+﻿using BonusBot.Common;
+using BonusBot.Common.Extensions;
 using BonusBot.Common.Helper;
 using BonusBot.Common.Interfaces.Guilds;
 using BonusBot.Common.Interfaces.Services;
@@ -25,14 +26,16 @@ namespace BonusBot.WebDashboardModule.Services
             var allAssemblyDatas = _modulesHandler.LoadedModuleAssemblies
                 .Where(SettingsHelper.HasSettings)
                 .Select(a => new ModuleData(a.ToModuleName(), bonusGuild.Modules.Contains(a)))
+                .Union(new List<ModuleData> { new(typeof(CommonSettings).GetModuleName(), true) })
                 .OrderBy(a => a.Name);
+
             return allAssemblyDatas;
         }
 
         public async Task<IEnumerable<ModuleSetting>> GetModuleSettings(string guildId, string moduleName)
         {
             var bonusGuild = _guildsHandler.GetGuild(ulong.Parse(guildId))!;
-            var assembly = bonusGuild.Modules.GetActivatedModuleAssembly(moduleName);
+            var assembly = moduleName != typeof(CommonSettings).GetModuleName() ? bonusGuild.Modules.GetActivatedModuleAssembly(moduleName) : typeof(CommonSettings).Assembly; 
             if (assembly is null) return Enumerable.Empty<ModuleSetting>();
 
             var settings = SettingsHelper.GetSettingKeys(assembly).Select(key => new ModuleSetting(key)).ToList();
