d4e68f6c8d514703bc3a0641fc31faf406d81005
emre1702 <emre1702@live.de>
2021-03-29 23:33:03 +0200

    Maybe fix the "Index was not found" error

--- Modules/WebDashboardModule/Views/Error/Error.cshtml.cs
+++ Modules/WebDashboardModule/Views/Error/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BonusBot.WebDashboardModule.Pages
+namespace BonusBot.WebDashboardModule.Views.Error
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
