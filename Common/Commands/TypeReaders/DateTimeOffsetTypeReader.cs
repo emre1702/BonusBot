88c928cdd687231a4c613902568cd17f94676a4e
emre1702 <emre1702@live.de>
2021-04-05 09:24:35 +0200

    Fix DateTimeOffsetTypeReader

--- Common/Commands/TypeReaders/DateTimeOffsetTypeReader.cs
+++ Common/Commands/TypeReaders/DateTimeOffsetTypeReader.cs
@@ -9,34 +9,48 @@ namespace BonusBot.Common.Commands.TypeReaders
 {
     public class DateTimeOffsetTypeReader : TypeReader
     {
-        public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider _)
         {
             if (!DateTime.TryParse(input, out var dateTime))
                 return TypeReaderResult.FromError(CommandError.ParseFailed, string.Format(Texts.CommandInvalidDateTimeOffsetError, input));
 
-            var (result, timeZone) = await GetDateTimeWithGuildTimezone(dateTime, (ICustomCommandContext)context);
+            bool hasTimeZoneInfo = HasTimeZoneInfo(dateTime);
+            dateTime = dateTime.ToUniversalTime();
+            var (result, timeZone) = await GetDateTimeWithGuildTimezone((ICustomCommandContext)context, dateTime, hasTimeZoneInfo);
             if (result is null)
                 return TypeReaderResult.FromError(CommandError.Unsuccessful, string.Format(Texts.InvalidTimeZoneIdError, timeZone));
             return TypeReaderResult.FromSuccess(result);
         }
 
-        private async Task<(DateTimeOffset?, string)> GetDateTimeWithGuildTimezone(DateTime dateTime, ICustomCommandContext context)
+        private async Task<(DateTimeOffset?, string)> GetDateTimeWithGuildTimezone(ICustomCommandContext context, DateTime dateTime, bool hasTimeZoneInfo)
         {
-            string timeZone = "UTC";
+            var (utcOffset, timeZoneName) = await GetTimeZoneOffset(context, dateTime);
+
+            if (hasTimeZoneInfo)
+                dateTime = dateTime.AddMinutes(utcOffset.TotalMinutes);
+            else 
+                dateTime = dateTime.AddMinutes(TimeZoneInfo.Local.GetUtcOffset(dateTime).TotalMinutes);
+            dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified);
+            var dateTimeOffset = new DateTimeOffset(dateTime, utcOffset);
+            return (dateTimeOffset, timeZoneName);
+        }
+
+        private async ValueTask<(TimeSpan, string)> GetTimeZoneOffset(ICustomCommandContext context, DateTime dateTime)
+        {
             if (context.BonusGuild is { })
             {
                 var timeZoneSetting = await context.BonusGuild.Settings.Get<string>(typeof(CommonSettings).Assembly, CommonSettings.TimeZone);
                 if (timeZoneSetting is { })
-                    timeZone = timeZoneSetting;
+                {
+                    if (TZConvert.TryGetTimeZoneInfo(timeZoneSetting, out var timeZoneInfo))
+                        return (timeZoneInfo.GetUtcOffset(DateTime.Now), timeZoneSetting);
+                }
+                    
             }
-
-            if (!TZConvert.TryGetTimeZoneInfo(timeZone, out var timeZoneInfo))
-                return (null, timeZone);
-
-            dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified);
-            var utcOffset = timeZoneInfo.GetUtcOffset(DateTime.Now);
-            var dateTimeOffset = new DateTimeOffset(dateTime.AddMinutes(utcOffset.TotalMinutes), utcOffset);
-            return (dateTimeOffset, timeZone);
+            return (HasTimeZoneInfo(dateTime) ? TimeZoneInfo.Local.GetUtcOffset(dateTime) : TimeSpan.Zero, "?");
         }
+
+        private bool HasTimeZoneInfo(DateTime dateTime)
+            => dateTime.Kind == DateTimeKind.Local;
     }
 }
