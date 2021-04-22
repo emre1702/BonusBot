0cc6fffd537ac4254c8461ac1159556db2565408
emre1702 <emre1702@live.de>
2021-04-22 17:24:46 +0200

    Fix selectedGuildId getting triggered multiple times

--- Modules/WebDashboardModule/ClientApp/src/app/modules/page/services/guild-selection.service.ts
+++ Modules/WebDashboardModule/ClientApp/src/app/modules/page/services/guild-selection.service.ts
@@ -1,21 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { merge, ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
 import api from 'src/app/routes/api';
 import { GuildData } from '../models/guild-data';
 
 @Injectable({ providedIn: 'root' })
 export class GuildSelectionService {
+    private selectedGuildId = new ReplaySubject<string>(1);
+    selectedGuildId$ = this.selectedGuildId.asObservable();
+
     private guilds = new ReplaySubject<GuildData[]>();
     guilds$ = this.guilds.asObservable();
 
-    private selectedGuildId = new ReplaySubject<string>(1);
-    selectedGuildId$ = merge(this.selectedGuildId, this.guilds.pipe(map((guilds) => guilds[0]?.id)));
-
     constructor(httpClient: HttpClient) {
         httpClient.get<GuildData[]>(api.get.navigation.guilds).subscribe((guilds) => {
-            if (guilds) this.guilds.next(guilds);
+            if (guilds) {
+                this.guilds.next(guilds);
+                this.selectGuildId(guilds[0]?.id);
+            }
         });
     }
 
